307362694bdf7718a3ac74436c1578c7015fa167
veraph <jmw3531@live.com>
2020-06-30 14:43:24 +0100

    finish the ch2.

--- ch2/struct.cc
+++ ch2/struct.cc
@@ -1,13 +1,34 @@
 // struct.cc -- the property of struct
 #include <iostream>
-
-struct Sales_data {
-    std::string book_no;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
-};
-
+#include <string>
+#include "Sales_data.h"
 int main()
 {
+    using namespace std;
+
+    Sales_data total;
+    double total_price;
+
+    if (cin >> total.book_no >> total.units_sold >> total_price) {
+        total.revenue = total_price * total.units_sold;
+
+        Sales_data trans;
+        double trans_price;
+        while (cin >> trans.book_no >> trans.units_sold >> trans_price) {
+            trans.revenue = trans_price * trans.units_sold;
+            if (total.book_no == trans.book_no){
+                total.units_sold += trans.units_sold;
+                total.revenue += trans.revenue;
+            } else {
+                cout << total.book_no << total.units_sold << total.revenue << endl;
+                total = trans;
+            }                
+        }
+        // print the last item
+        cout << total.book_no << total.units_sold << total.revenue << endl;
+    } else {
+        cerr << "Data Need!" << endl;
+        return -1;
+    }
     return 0;
 }
