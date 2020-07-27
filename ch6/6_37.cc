b19c9dededc9cee74909fd7719fca5979d74167b
Veraph <jmw3531@live.com>
2020-07-27 22:16:28 +0100

    add more excercise of ch6.

---
+++ ch6/6_37.cc
@@ -0,0 +1,15 @@
+// 6_37.cc -- return arrary's references and pointer
+#include <iostream>
+#include <string>
+using std::string;
+
+// method1
+using ArrT = string[10];
+ArrT &func1(ArrT &arr);
+
+// method2
+auto func2(ArrT &arr) -> string(&) [10];
+
+// method3
+string arrS[10];
+decltype(arrS) &func3(ArrT &arr);
